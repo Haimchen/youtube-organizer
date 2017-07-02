92b1559a282fc5bc73ea92029ca6a141a40dacbd
Sarah Köhler <sarah.c.koehler@gmail.com>
2017-07-02 16:51:33 +0200

    add route for collections

---
+++ src/routes/Collections/components/CollectionsOverview.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export const CollectionsOverview = (props) => (
+    <div>
+      <h1>My Collections</h1>
+      <p>You do not have any collections yet.</p>
+    </div>
+)
+CollectionsOverview.propTypes = {
+}
+
+export default CollectionsOverview
+
---
+++ src/routes/Collections/containers/CollectionsContainer.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux'
+
+import CollectionsOverview from '../components/CollectionsOverview'
+
+const mapDispatchToProps = {
+}
+
+const mapStateToProps = (state, props) => ({
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CollectionsOverview)
+
